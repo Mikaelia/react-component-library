feb2a15bff6f6b83dd78b0d55ef02a4e148581b4
Mikaela Gurney <354@holbertonschool.com>
2019-01-17 14:07:57 -0800

    Install enzyme and add initial test

---
+++ src/components/TextInputBEM/index.js
@@ -0,0 +1,5 @@
+import "./textInput.css";
+export { default } from "./TextInputBEM";
+
+// If want styled component, import from here, it not
+// import component file
---
+++ src/docs/examples/TextInputBEM/ExampleError.js
@@ -0,0 +1,18 @@
+import React from "react";
+import TextInput from "component-library/TextInputBEM";
+
+/** Error TextBox */
+export default class ExampleErrorBEM extends React.Component {
+  render() {
+    return (
+      <TextInput
+        htmlId="example-error"
+        label="First Name"
+        name="firstname"
+        onChange={() => {}}
+        required
+        error="First name is required"
+      />
+    );
+  }
+}
